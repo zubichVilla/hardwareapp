c05d82b00c8e6ffd9061022f299929c51c842b3b
zube <zubcic.toni@hotmail.com>
2022-04-20 18:36:28 +0200

    CORS Added

--- src/main/java/hr/tvz/zubcic/hardwareapp/hardware/HardwareRepositoryImpl.java
+++ src/main/java/hr/tvz/zubcic/hardwareapp/hardware/HardwareRepositoryImpl.java
@@ -11,7 +11,7 @@ public class HardwareRepositoryImpl implements HardwareRepository {
     private final List<Hardware> HARDWARE_LIST = new ArrayList<>(Arrays.asList(
             new Hardware("ASUS GTX1650", "097.100.990", BigDecimal.valueOf(2400.50),
                     Hardware.Type.GPU, 20),
-            new Hardware("DDR3 4GB (1x4) Corsair 1333MHz", "096.100.221", BigDecimal.valueOf(221.10),
+            new Hardware("DDR3 4GB (1x4) Corsair", "096.100.221", BigDecimal.valueOf(221.10),
                     Hardware.Type.RAM, 5),
             new Hardware("Intel Core i5-10600K 4.1GHz", "091.900.007", BigDecimal.valueOf(2181.10),
                     Hardware.Type.CPU, 10)
